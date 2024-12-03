8e549dc757d28555b93ae56e2867a094e765a79c
federico-init <fefe.borrometi@gmail.com>
2024-12-03 08:57:40 +0100

    chore: filter car list based on search term

--- src/components/CarList.jsx
+++ src/components/CarList.jsx
@@ -1,12 +1,16 @@
 import { useDispatch, useSelector } from "react-redux";
+
 import { deleteCar } from "../store";
 
 function CarList() {
   const dispatch = useDispatch();
 
-  const cars = useSelector((state) => {
-    return state.cars.list;
+  const cars = useSelector(({ cars: { list, searchTerm } }) => {
+    return list.filter((car) =>
+      car.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
   });
+  const name = useSelector((state) => state.form.name);
 
   const handleCarDelete = (car) => {
     dispatch(deleteCar(car.id));
@@ -16,7 +20,7 @@ function CarList() {
     return (
       <div key={car.id} className="panel">
         <p>
-          {car.name} - ${car.cost}
+          {car.name} - €{car.cost}
         </p>
         <button
           className="button is-danger"
